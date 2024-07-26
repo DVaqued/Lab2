427001d628dbf1fb36623559f6debdd74c39e9f8
angel <angel@dvaqued>
2024-07-26 10:32:39 -0600

    Importo el escaner, defino cada una de las variables para poder usar los whiles y se actualice constantemente, cuando este mismo while llegue a la cantidad de notas que se le indique, luego se calcula el promedio y las notas mayores y menores.

---
+++ src/vaquedano_angel_lab2/java/Vaquedano_Angel_Lab2.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vaquedano_angel_lab2.java;
+import java.util.Scanner;
+
+/**
+ *
+ * @author angel
+ */
+public class Vaquedano_Angel_Lab2{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int ejer=0;
+        while (ejer!=4){
+        
+        Scanner lea=new Scanner(System.in).useDelimiter("\n");
+        
+        System.out.println("---Menú---");
+        System.out.println("Ingrese 1 si quiere ir al ejercicio CADENA");
+        System.out.println("Ingrese 2 si quiere ir al ejercicio NOTAS");
+        System.out.println("Ingrese 3 si quiere ir al ejercicio CLASES");
+        
+        System.out.println("Favor ingrese el número del ejercicio que desea realizar: ");
+        ejer=lea.nextInt();
+        
+        
+        
+        if (ejer==1){
+        
+    }
+        
+        else if (ejer==2){
+            int contador=1;
+            double nota=0;
+            int cantidadnotas=0;
+            double sumatotal=0;
+            double promedio=0;
+            double notamayor=0;
+            double notamenor=100;
+            
+           
+                
+            System.out.println("Favor ingrese la cantidad de notas: ");
+            cantidadnotas=lea.nextInt();
+            while (cantidadnotas>1){
+            while (cantidadnotas>=contador){
+                System.out.println("Favor ingrese la nota "+contador+": ");
+                nota=lea.nextDouble();
+                
+                
+                if (nota>=0 && nota<=100){
+                   contador+=1;
+                   sumatotal+=nota;
+                   
+                   while(nota>notamayor){
+                       notamayor=nota;
+                       
+                   }
+                   while(nota<notamenor){
+                       notamenor=nota;
+                   }
+                   promedio=sumatotal/contador;
+                }
+            }      
+            }
+            
+            System.out.println("---Resultados---"+
+                    "\n Su promedio es: "+promedio+
+                    "\n Su nota mayor fue: "+notamayor+
+                    "\n Su nota menor fue: "+notamenor);
+        }
+        
+        else if(ejer==3){
+            
+        }
+        }
+    }
+}
